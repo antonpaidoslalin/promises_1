db2816bdf3fb76cd77630c49722fc613cc5915dd
Antón Vega <anton.paidoslalin@gmail.com>
2022-03-02 13:36:41 +0100

    Solucionar cambios no guardados en archivos para promesas

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import './styles.css';
-import { buscarHeroe } from "./js/callbacks.js";
+// import { buscarHeroe } from "./js/callbacks.js";
 const heroeID='capi';
-buscarHeroe(heroeID, (err, heroe) => {
-    // console.log('Chamamos callback');
-    if (err) {
-        console.error(err);
-    } else {
-        console.info(heroe);
-    }
-})
+// buscarHeroe(heroeID, (err, heroe) => {
+//     // console.log('Chamamos callback');
+//     if (err) {
+//         console.error(err);
+//     } else {
+//         console.info(heroe);
+//     }
+// })
---
+++ src/js/promesas.js
@@ -0,0 +1,14 @@
+const heroes = {
+    capi:{
+        nombre: 'Capitán América',
+        poder: 'Aguantar inyecciones sin morir'
+    },
+    iron: {
+        nombre:'Ironman',
+        poder:'Una absurda cantidad de dinero'
+    },
+    spider:{
+        nombre:'Spiderman',
+        poder:'La mejor reacción alérgica a las picaduras de araña'
+    }
+}
