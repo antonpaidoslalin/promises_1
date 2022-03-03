93e1b63b6e48072bc17ab92d8dd859d048ed5b50
Antón Vega <anton.paidoslalin@gmail.com>
2022-03-03 11:21:14 +0100

    Promesa multiple con Promise.all

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './styles.css';
  import { buscarHeroe as buscarHeroeCallback } from "./js/callbacks.js";
  import { buscarHeroe } from './js/promesas';
-const heroeID='capi';
+const heroeId1='capi';
+const heroeId2='iron';
+const heroeId3='spider';
 // buscarHeroe(heroeID, (err, heroe) => {
 //     // console.log('Chamamos callback');
 //     if (err) {
@@ -10,3 +12,17 @@ const heroeID='capi';
 //         console.info(heroe);
 //     }
 // })
+// ==============PROMESA V.0====================
+// buscarHeroe (heroeId1).then((heroe) => {
+//     console.log(`Enviando ${heroe.nombre} á misión`);
+// });
+// console.log('Fin do programa');
+
+//===============PROMESA V.1=====================
+
+Promise.all([buscarHeroe(heroeId1),buscarHeroe(heroeId2), buscarHeroe(heroeId3)]).then(heroes => {
+    for (let i=0; i < heroes.length; i++){
+    console.log(heroes[i].nombre);
+    }
+})
+
